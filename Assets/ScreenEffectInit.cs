c8c0cb239c295f2c2a4549ed1aabf3c751f8e4b8
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-02-10 11:26:50 +0800

    实现黑屏

---
+++ Assets/ScreenEffectInit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ScreenEffectInit : MonoBehaviour
+{
+
+	void Awake ()
+    {
+        VRTK_SharedMethods.AddCameraFade();
+    }
+    //实现黑屏
+    //VRTK_SDK_Bridge.HeadsetFade(Color.black, 0f);
+    //VRTK_SDK_Bridge.HeadsetFade(Color.clear, 4f);
+}
